05e3c7bee9e3d9fcb95b92c7dfccc8645c4c22f1
gokhnesen <gokhan_esen@hotmail.com.tr>
2023-03-18 19:02:21 +0300

    Adding Palindrome

---
+++ Palindrome/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Palindrome
+{
+    public class Program
+    {
+        public static string isPalindrome(string str)
+        {
+
+            // Insert your code here
+            string reverse = string.Empty;
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                reverse += str[i];
+            }
+            if (str == reverse)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+            
+            return reverse;
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(isPalindrome("kayak"));
+        }
+    }
+}
