8a572a6dfb25aa3abaa178c19acaa56fa09456f7
gokhnesen <gokhan_esen@hotmail.com.tr>
2023-03-28 18:36:31 +0300

    TwoSum

---
+++ TwoSum/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace TwoSum
+{
+    public class Program
+    {
+        public static int[] TwoSum(int[] numbers, int target)
+        {
+            int index=0, index2=numbers.Length-1;
+            while (numbers[index] + numbers[index2] != target)
+            {
+                if (numbers[index] + numbers[index2] > target)
+                {
+                    index2--;
+                }
+                else index++;
+            }
+            return new int[] {index+1,index2+1};
+        }
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            TwoSum(numbers, 3);
+        }
+    }
+}
