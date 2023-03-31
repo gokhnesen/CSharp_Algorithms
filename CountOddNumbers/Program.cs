145ee27d0e900f62acceffea633081e31ebd34d4
gokhnesen <gokhan_esen@hotmail.com.tr>
2023-03-31 16:18:05 +0300

    CountOddNumbers solutions

---
+++ CountOddNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace CountOddNumbers
+{
+    internal class Program
+    {
+        //https://leetcode.com/problems/count-odd-numbers-in-an-interval-range
+        public int CountOdds(int low, int high)
+        {
+            int odd = 0;
+
+            for(int i=low;i<high;i++)
+            {
+                if(i%2 == 0)
+                {
+                    odd++;
+                }
+            }
+            return odd;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
