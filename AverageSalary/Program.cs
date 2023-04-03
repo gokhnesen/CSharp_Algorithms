f81c435a26eb0b7644618c625d51e6ea992c8145
gokhnesen <gokhan_esen@hotmail.com.tr>
2023-04-03 18:14:50 +0300

    AverageSalary solution

---
+++ AverageSalary/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace AverageSalary
+{
+    public class Program
+    {
+        //https://leetcode.com/problems/average-salary-excluding-the-minimum-and-maximum-salary/
+        public double Average(int[] salary)
+        {
+            int sum = 0;
+            int average = 0;
+            for (int i = 0; i < salary.Length; i++)
+            {
+                sum += salary[i];
+            }
+            return sum/salary.Length;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
