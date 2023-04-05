3a9843b130cf0c53fbab4f0aac8cd1f3ebed06d9
gokhnesen <gokhan_esen@hotmail.com.tr>
2023-04-05 23:54:30 +0300

    Some algo solutions

---
+++ SignOfTheProductOfAnArray/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace SignOfTheProductOfAnArray
+{
+    internal class Program
+    {
+        //https://leetcode.com/problems/sign-of-the-product-of-an-array
+        public int ArraySign(int[] nums)
+        {
+            var sum = 1;
+            for(int i=0;i<=nums.Length; i++)
+            {
+                sum *= nums[i];
+            }
+            return signFunc(sum);
+        }
+
+        public int signFunc(int product)
+        {
+            int result=0;
+            if(product == 0)
+            {
+                result= 0;
+            }
+            if(product >= 1)
+            {
+                 result= 1;
+            }
+            if(product < 0)
+            {
+                result= -1;
+            }
+            return result;
+        }
+
+       
+        static void Main(string[] args)
+        {
+            Console.WriteLine(5 * 6 * -5);
+        }
+    }
+}
